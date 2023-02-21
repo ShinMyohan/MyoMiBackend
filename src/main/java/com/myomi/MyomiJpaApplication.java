d5cc4a3954cd0fd309d5c54644a79abf8b408b98
taeheev <hailey_kim_@naver.com>
2023-02-21 16:18:36 +0900

    Feat: Delete entities to merge

--- src/main/java/com/myomi/MyomiJpaApplication.java
+++ src/main/java/com/myomi/MyomiJpaApplication.java
@@ -2,6 +2,9 @@ package com.myomi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MyomiJpaApplication {
@@ -9,5 +12,16 @@ public class MyomiJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyomiJpaApplication.class, args);
 	}
+	
+	// CORS 설정
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:5500");
+            }
+        };
+    }
 
 }
--- src/main/java/com/myomi/user/entity/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.myomi.user.entity;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.myomi.user.entity.User;
-
-public interface UserRepository extends CrudRepository<User, String>{
-
-}
